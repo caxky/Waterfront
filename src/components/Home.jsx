c3e244146058aafde2da3dc2a0d784ae842a09c8
caxky <k_james7@outlook.com>
2022-06-02 18:15:41 -0400

    Add wave background to splashscreen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,20 @@ import { Button, Col } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <Col className='mx-auto text-center'>
-      <img
-        src='logo.svg'
-        width='200'
-        height='200'
-        className="d-inline-block align-top"
-        alt="Waterfront management logo"
-      ></img>
-      <h1 className='mt-4'>Wealthfront Management</h1>
-      <h3>Financial solutions and wealth planning</h3>
-      <Button className="mt-5" href="#book">
-        GET STARTED
-      </Button>
-      <div class="down-arrow"></div>
-    </Col>
+    <div className='home'>
+      <Col className='mx-auto text-center'>
+        <img
+          src='logo.svg'
+          className="d-inline-block align-top home-logo"
+          alt="Waterfront management logo"
+        ></img>
+        <h1 className='mt-4'>Wealthfront Management</h1>
+        <h3>Financial solutions and wealth planning</h3>
+        <Button className="mt-5" variant="outline-dark" href="#book">
+          GET STARTED
+        </Button>
+        <div class="down-arrow"></div>
+      </Col>
+    </div>
   )
 }
