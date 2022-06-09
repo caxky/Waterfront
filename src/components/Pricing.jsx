890eab68446c8060037784891f1d9d5d425f705b
caxky <k_james7@outlook.com>
2022-06-08 21:01:46 -0400

    Addition content for pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,5 @@
 import './Pricing.scss';
-import { Row, Col, Card, ListGroup } from 'react-bootstrap';
+import { Row, Col, Card, ListGroup, Button } from 'react-bootstrap';
 import { CheckCircle, XCircle } from 'react-bootstrap-icons';
 
 export default function Pricing() {
@@ -7,7 +7,7 @@ export default function Pricing() {
     <div className='my-5 py-2 pricing' id='pricing'>
       <h2 className='text-center my-5'>PRICING</h2>
       <Row className='d-flex'>
-        <Col>
+        <Col className='mb-5' sm={12} xl={4}>
         <Card>
           <Card.Body>
             <Card.Title className='text-center' id='card-1'>$0 - $249,999</Card.Title>
@@ -28,7 +28,7 @@ export default function Pricing() {
         </Card>
         </Col>
 
-        <Col>
+        <Col className='mb-5' sm={12} xl={4}>
           <Card>
             <Card.Body>
               <Card.Title className='text-center' id='card-2'>$250,000 - $749,999</Card.Title>
@@ -42,14 +42,14 @@ export default function Pricing() {
                 <ListGroup.Item><XCircle className='me-1'/>Tax Planning</ListGroup.Item>
               </ListGroup>
               <Card.Text className='text-center'>
-                <h5><b>1.15%</b></h5>
+                <h5><b>0.85%</b></h5>
                 <h5>Annual Management Fee</h5>
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
 
-        <Col>
+        <Col className='mb-5' sm={12} xl={4}>
           <Card>
             <Card.Body>
               <Card.Title className='text-center' id='card-3'>$750,000 - $1,499,999</Card.Title>
@@ -63,12 +63,17 @@ export default function Pricing() {
                 <ListGroup.Item><CheckCircle className='me-1'/>Tax Planning</ListGroup.Item>
               </ListGroup>
               <Card.Text className='text-center'>
-                <h5><b>1.15%</b></h5>
+                <h5><b>0.45%</b></h5>
                 <h5>Annual Management Fee</h5>
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
+      </Row>
+
+      <Row className='mx-auto mb-5 text-center wrapper'>
+        <h4 className='mb-3'>Need to manage more than $1.5M?</h4>
+        <Button>CONTACT US</Button>
       </Row>
     </div>
   )
